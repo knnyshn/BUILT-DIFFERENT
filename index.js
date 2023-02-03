611e0d2664bb49e1b7c4c8762293a514affb5b5a
Kenny <knnyshn@gmail.com>
2023-02-03 12:53:24 -0500

    css time

--- index.js
+++ index.js
@@ -20,13 +20,13 @@ async function champAPI() {
   const json = await res.json();
   let adArray = [];
   for(let i in json.data){
-    if(json.data[i].tags.includes('Marksman') && !json.data[i].tags.includes('Mage')){
+    if(json.data[i].tags.includes('Marksman') && !json.data[i].tags.includes('Mage') && !json.data[i].tags.includes('Fighter')){
       adArray.push([i, json.data [i]]);
     }
   }
-  let randomAd = Math.floor(Math.random() * ((adArray.length) - 0) + 0);
-  console.log(adArray);
-  console.log(adArray[randomAd][0]);
+  let randomAd = Math.floor(Math.random() * ((adArray.length))); 
+  // console.log(adArray);
+  // console.log(adArray[randomAd][0]);
   return (adArray[randomAd][0]);
 }
 champAPI();
@@ -35,33 +35,34 @@ champAPI();
 // ["Aphelios"]["All Aphelios Details"]
 
 async function itemAPI() {
-  try {
-    const res = await fetch(items_base);
-    console.log(res);
-    const json = await res.json();
-    let itemArray = [];
-    for(let i in json.data){
-      if(json.data[i].tags.includes('CriticalStrike') && json.data[i].gold.total > 2000){
-        itemArray.push([i, json.data [i]]);
-      }
+  const res = await fetch(items_base);
+  // console.log(res);
+  const json = await res.json();
+  let itemArray = [];
+  for(let i in json.data){
+    if(json.data[i].tags.includes('CriticalStrike') && json.data[i].gold.total > 2000){
+      itemArray.push([i, json.data [i]]);
     }
-    console.log(itemArray);
-    console.log(Object.keys(json.data));
-    console.log('json       ', json);
-    console.log('json.data  ', json);
-    const items = Object.keys(json.data);
-  } catch (error) {
-    console.log(error);
-    // Sanity Checks
   }
-}
+  let itemData = [];
+  for(let i = 0; i < 6; i++) {
+    console.log(itemArray[i][0]);
+    itemData.push(itemArray[i][0]);
+  }
+    // console.log(itemArray);
+    // console.log(Object.keys(json.data));
+    // console.log('json       ', json);
+    // console.log('json.data  ', json);
+    // Sanity Checks
+    const items = Object.keys(json.data);
+    return itemData;
+  }
 itemAPI();
 // Fetch items that have specific filters for AD champions (generalizing) that are endgame / completed items.
 
 async function summSpellAPI() {
-  try {
     const res = await fetch(summoner_spell_base);
-    console.log(res);
+    // console.log(res);
     const json = await res.json();
     let summSpellArray = [];
     for(let i in json.data){
@@ -72,45 +73,52 @@ async function summSpellAPI() {
         summSpellArray.push([i, json.data [i]]);
       }
     }
-    console.log(summSpellArray[0]);
-    return summSpellArray;
-  } catch (error) {
-    console.log(error);
+    // console.log(summSpellArray[0]);
     // Sanity Checks
+    return summSpellArray;
+
   }
  
-}
 // Summoner Spell API
-
 const button = document.getElementById("reroll");
+
 button.addEventListener("click", async () => {
   const champ = await champAPI()
   document.body.getElementsByClassName("champSplash")[0].innerHTML='';
-  document.body.getElementsByClassName("champSplash")[0].innerHTML='';
   const champIcon = "http://ddragon.leagueoflegends.com/cdn/13.1.1/img/champion/"+ champ +".png";
-  console.log(champIcon);
+  // console.log(champIcon);
   const span = document.createElement("span");
   span.innerText = champ; 
   const img = document.createElement("img");
   img.src = champIcon;
-  img.alt = champ;
   document.body.getElementsByClassName("champSplash")[0].appendChild(img);
   document.body.getElementsByClassName("champSplash")[0].appendChild(span);
-  const sillyTitle = ["GIGACHAD", "FREAK OF NATURE", "TRULY BUILT DIFFERENT", "JOHN WICK'S", "BIG TONKA TRUCK", "IDGAF", "", "6'4 and 280 POUNDS", "MASSIVE PROBLEM", "ABSOLUTE MONSTER", "DEADLIFTS 550 POUNDS"];
-  let randTitle = Math.floor(Math.random() * ((sillyTitle.length) - 0) + 0);
-  console.log(randTitle);
-  console.log((sillyTitle[randTitle]));
-  document.getElementById("title").innerHTML = sillyTitle[randTitle];
+  const sillyTitle = ["GIGACHAD", "FREAK OF NATURE", "TRULY BUILT DIFFERENT", "JOHN WICK'S", "BIG TONKA TRUCK", "IDGAF", "6'4 and 280 POUNDS", "MASSIVE PROBLEM", "ABSOLUTE MONSTER", "2000 IQ"];
+  let randTitle = Math.floor(Math.random() * ((sillyTitle.length)));
+  // console.log(randTitle);
+  // console.log((sillyTitle[randTitle]));
+  document.getElementById("title").innerHTML=sillyTitle[randTitle];
 
-  const summSpells = await summSpellAPI()
+  const item = await itemAPI();
+  console.log(item);
+  for (let i = 0; i < item.length; i++) {
+    const icon = "http://ddragon.leagueoflegends.com/cdn/13.1.1/img/item/"+ item[i] +".png";
+    const img3 = document.createElement("img");
+    img3.src = icon;
+    img3.alt = item[i];
+    document.body.getElementsByClassName("item")[i].innerHTML = "";
+    document.body.getElementsByClassName("item")[i].appendChild(img3);
+  }
+
+  const summSpells = await summSpellAPI();
   console.log({summSpells});
   const spellsDivs = document.querySelectorAll('.spells')
   for (let i = 0; i < summSpells.length; i++) {
-    const [name, props] = summSpells[i]
-    const spellDiv = spellsDivs[i]
+    const [name, props] = summSpells[i];
+    const spellDiv = spellsDivs[i];
     spellDiv.innerHTML = '';
     const summSpellIconOne = "http://ddragon.leagueoflegends.com/cdn/13.1.1/img/spell/"+ name +".png";
-    console.log(summSpellIconOne);
+    // console.log(summSpellIconOne);
     // const span2 = document.createElement("span");
     // span2.innerText = name;
     const img2 = document.createElement("img");
@@ -119,8 +127,13 @@ button.addEventListener("click", async () => {
     spellDiv.append(img2);
   }
 
+
+
+  
+  
 });
   // Item linker here???
+  // Summoner Spell generator here???
 
 
 // Button that is truly built different.
